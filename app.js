b665ec455bc2eb996927f0cdcfa89dd55b53901e
antonpitiulin <antonpitiulin@gmail.com>
2019-02-09 20:56:21 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("hello")
+
+var x = 5
+
+console.log(x)
+
+const name = "anton"
+
+console.log(x + name)
+
+const array = [3,3,4,"same"]
+
+console.log(array[1])
+
+function sum(x,y){
+    return x + y
+}
+sum(4,5)
+
+console.log(sum(21,3))
+
+function addPreficx(name){
+    return "mr. "+ name
+}
+
+console.log(addPreficx("roma"))
